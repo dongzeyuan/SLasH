55ef6cff69fc598e893622c423f16c0fd78f8351
dongzeyuan <dongfujing88@gmail.com>
2018-08-20 16:19:28 +0800

    有一点门路了

--- probe.py
+++ probe.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
+# @Time    : 2018/08/20
+# @Author  : DongFJ
+# @Email   : sjsgydfj@163.com
+# @File    : probe.py
+# @Software: VS Code
 '''
-分析输入的CSV文件，生成腐蚀监测数据分析结果
+Description : 分析输入的CSV文件，图表呈现
+主要内容：
+
 20,SN:1600001,Tag NO:RE-0001,0.504mm
 1,2017,2,15,8,0,50000,50000
 2,2017,2,15,10,0,49900,50000
 CSV文件格式如上所示
 '''
+from datetime import datetime
+import pprint
 
 
 def getData(fileName):
-    '''输入文件名，返回时间，测试电阻，参考电阻'''
+    '''输入文件名，返回时间，测试电阻，参考电阻3个列表'''
 
     dataFile = open(fileName, 'r')
     times = []
@@ -25,14 +34,33 @@ def getData(fileName):
     li8 = []
     dataFile.readline()  # ignore header
     for line in dataFile:
-        li1,li2,li3,li4,li5,li6,li7,li8 = line.split(',')
-        times.append(str(li2+li3+li4+li5+li6))
+        li1, li2, li3, li4, li5, li6, li7, li8 = line.split(',')
+        times.append(str(li2 + '-' + li3 + '-' + li4 + '-' + li5 + '-' + li6))
         resElements.append(float(li7))
         resRefers.append(float(li8))
     dataFile.close()
-    return (times,resElements,resRefers)
+    return (times, resElements, resRefers)
+
+
+def calcData(inputFile, oriThick):
+    '''输入文件名，返回时间序列，metal loss序列'''
+    timestamp = []
+    resRatio = []
+    metalloss = []
+    times, resElements, resRefers = getData(inputFile)
+    for time_str in times:
+        time = datetime.strptime(time_str, '%Y-%m-%d-%H-%M')
+        time.strftime('%Y-%m-%d-%H-%M')
+        timestamp.append(time)
+    for i in range(len(resElements)):
+        ratio = resRefers[i] / resElements[i]
+        resRatio.append(ratio)
+    for i in range(len(resRatio)):
+        lose = (oriThick - oriThick * resRatio[i])
+        metalloss.append(lose)
+    return (timestamp, metalloss)
 
 
 if __name__ == "__main__":
-    print(getData('D:\code\SLasH\ER-0001.csv'))
-    
+    pprint.pprint(getData('D:\code\SLasH\ER-0001.csv'))
+    pprint.pprint(calcData('D:\code\SLasH\ER-0001.csv', 0.508))
