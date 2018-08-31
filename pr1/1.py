e7261d3af31294de4b615426af729bdff409b43d
dongzeyuan <dongfujing88@gmail.com>
2018-08-31 10:55:25 +0800

    18-8-31 有始有终了

--- pr1/1.py
+++ pr1/1.py
@@ -1,8 +1,8 @@
 class MonPl(object):
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, ap):
         self.name = name
         self.hp = hp
-        self.ap = 1
+        self.ap = ap
         print('{0} has joinned the battle'.format(self.name))
 
     def getstatus(self):
@@ -10,11 +10,14 @@ class MonPl(object):
 
 
 def fight(player, monster):
+    while monster.getstatus() > 0:
         monster.hp = monster.hp - player.ap
         print('The monster has {0} hp left'.format(monster.getstatus()))
+    else:
+        print('The monster has been defeated')
 
 
 if __name__ == "__main__":
-    player = MonPl('V',100)
-    monster = MonPl('GW', 100)
+    player = MonPl('V', 100, 3)
+    monster = MonPl('GW', 100, 1)
     fight(player, monster)
