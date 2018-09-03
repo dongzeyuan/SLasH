bf7fda85e1f0ff4ff6032dbd015f989eb72806b2
dongzeyuan <dongfujing88@gmail.com>
2018-09-03 16:52:42 +0800

    9-03 battle

--- pr1/1.py
+++ pr1/1.py
@@ -1,8 +1,11 @@
+import time
+
 class MonPl(object):
-    def __init__(self, name, hp, ap):
+    def __init__(self, name, hp, ap, attsp):
         self.name = name
         self.hp = hp
         self.ap = ap
+        self.attsp = attsp
         print('{0} has joinned the battle'.format(self.name))
 
     def getstatus(self):
@@ -13,11 +16,14 @@ def fight(player, monster):
     while monster.getstatus() > 0:
         monster.hp = monster.hp - player.ap
         print('The monster has {0} hp left'.format(monster.getstatus()))
+        time.sleep(player.attsp)
+        player.hp = player.hp - monster.ap
+        print('The player has {0} hp left'.format(player.getstatus()))
     else:
         print('The monster has been defeated')
 
 
 if __name__ == "__main__":
-    player = MonPl('V', 100, 3)
-    monster = MonPl('GW', 100, 1)
+    player = MonPl('V', 100, 3, 2)
+    monster = MonPl('GW', 100, 1, 5)
     fight(player, monster)
